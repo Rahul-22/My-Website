95a738db12220ab0f39531c95c467b0c7717c021
Rahul22 <rahul.marathervar17@siesgst.ac.in>
2021-07-13 18:05:11 +0530

    MyWebsite

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  public name = "Rahul Ganesh";
+  // public contactComponent: any = '';
+  title = 'hello-world';
+  
+
+  constructor(){}
+  
+}
